6622ae23fae963b83f6e46c09417527dfc196835
Ma <594408294@qq.com>
2020-05-26 11:40:38 +0800

    电影板块首页 即将上映视口完工

---
+++ app/src/main/java/com/uw/alice/ui/navigationB/MovieComingSoonAdapter.java
@@ -0,0 +1,99 @@
+package com.uw.alice.ui.navigationB;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.uw.alice.R;
+import com.uw.alice.data.model.Movie;
+
+import java.util.List;
+
+public class MovieComingSoonAdapter extends RecyclerView.Adapter <MovieComingSoonAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Movie.SubjectsBean> mlistBeans;
+    private OnItemClickListener onItemClickListener;
+
+
+    @NonNull
+    @Override
+    public MovieComingSoonAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        mContext = parent.getContext();
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie_coming_list, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MovieComingSoonAdapter.ViewHolder holder, int position)
+    {
+        Movie.SubjectsBean listBean = mlistBeans.get(position);
+        Glide.with(mContext).load(listBean.getImages().getLarge()).placeholder(R.mipmap.icon_placeholder).into(holder.ivMoviePoster);
+        holder.tvMovieName.setText(listBean.getTitle());
+        //holder.tvMovieScore.setText(String.valueOf(listBean.getRating().getAverage()));
+        holder.tvDate.setText(listBean.getMainland_pubdate().substring(5));
+
+        // 如果设置了回调，则设置点击事件
+        if (onItemClickListener != null)
+        {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    onItemClickListener.onItemClick(holder.itemView, pos);
+                }
+            });
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return mlistBeans.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView ivMoviePoster;
+        TextView tvMovieName ;
+        TextView tvMovieScore;
+        TextView tvDate;
+
+        ViewHolder(View view)
+        {
+            super(view);
+            ivMoviePoster = view.findViewById(R.id.iv_movie_poster);
+            tvMovieName = view.findViewById(R.id.tv_movie_name);
+            //tvMovieScore = view.findViewById(R.id.tv_movie_score);
+            tvDate = view.findViewById(R.id.tv_date);
+        }
+    }
+
+    MovieComingSoonAdapter(List<Movie.SubjectsBean> listBeans){
+        mlistBeans = listBeans;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener)
+    {
+        this.onItemClickListener = onItemClickListener;
+    }
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+
+
+
+
+
+
+}
