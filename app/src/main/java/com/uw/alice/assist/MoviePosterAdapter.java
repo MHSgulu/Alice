76fd1ac2c85f79f49cd1c910a1f8819bb2aa6cf8
Ma <594408294@qq.com>
2020-05-26 11:37:47 +0800

    增加海报测试数据和数据集适配器

---
+++ app/src/main/java/com/uw/alice/assist/MoviePosterAdapter.java
@@ -0,0 +1,53 @@
+package com.uw.alice.assist;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.uw.alice.data.model.MoviePoster;
+import com.youth.banner.adapter.BannerAdapter;
+
+import java.util.List;
+
+/**
+ * 自定义布局，下面是常见的图片样式，更多实现可以看demo，可以自己随意发挥
+ */
+public class MoviePosterAdapter extends BannerAdapter<MoviePoster, MoviePosterAdapter.BannerViewHolder> {
+
+    private Context mContext;
+
+    public MoviePosterAdapter(List<MoviePoster> moviePosterList) {
+        //设置数据，也可以调用banner提供的方法,或者自己在adapter中实现
+        super(moviePosterList);
+    }
+
+    //创建ViewHolder，可以用viewType这个字段来区分不同的ViewHolder
+    @Override
+    public BannerViewHolder onCreateHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        ImageView imageView = new ImageView(parent.getContext());
+        //注意，必须设置为match_parent，这个是viewpager2强制要求的
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        return new BannerViewHolder(imageView);
+    }
+
+    @Override
+    public void onBindView(BannerViewHolder holder, MoviePoster data, int position, int size) {
+        //holder.imageView.setImageResource(data.getImage());
+        Glide.with(mContext).load(data.getImage()).into(holder.imageView);
+    }
+
+    class BannerViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public BannerViewHolder(@NonNull ImageView view) {
+            super(view);
+            this.imageView = view;
+        }
+    }
+}
